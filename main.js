11dcda4e00d53dbfad0655153e5fb3ec83a8df09
Francis Obwogo <francobwogo@gmail.com>
2023-12-03 19:58:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// document.getElementById("email").addEventListener("input", function () {
+//     const email = this.value;
+
+//     if (!isValidEmail(email)) {
+//         this.setCustomValidity("Please enter a valid email address.");
+//     } else {
+//         this.setCustomValidity("");
+//     }
+// });
