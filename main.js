c590767061180a0fcfc15efb7b6d9b4aaf3b4f8f
Francis Obwogo <francobwogo@gmail.com>
2023-12-05 00:17:31 +0200

    css and javascript update

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-// document.getElementById("email").addEventListener("input", function () {
-//     const email = this.value;
+function validation() {
+  let form = document.getElementById("form");
+  let emailAdd = document.getElementById("email");
+  let email = emailAdd.value;
+  let text = document.getElementById("text");
+  let pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-//     if (!isValidEmail(email)) {
-//         this.setCustomValidity("Please enter a valid email address.");
-//     } else {
-//         this.setCustomValidity("");
-//     }
-// });
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Valid Email Required";
+    text.style.color = "#FF624F;";
+  }
+
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  }
+}
